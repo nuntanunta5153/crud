22ea6b0948b54770fd360d40db4cc7515e7b440f
nuntanunta5153 <nuntanunta5153@gmail.com>
2021-03-28 16:23:56 +0900

    1回目の回答

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,7 @@ class UsersController < ApplicationController
   
     def create
       User.create(user_params)
-      # 以下のメソッドを追加
-      redirect_to action: :index
+    #   redirect_to action: :index
     end
   
     def edit
@@ -21,13 +20,13 @@ class UsersController < ApplicationController
     def update
       user = User.find(params[:id])
       user.update(user_params)
-      redirect_to action: :index
+    #   redirect_to action: :index
     end
   
     def destroy
       user = User.find(params[:id])
       user.destroy
-      redirect_to action: :index
+    #   redirect_to action: :index
     end
   
     private
